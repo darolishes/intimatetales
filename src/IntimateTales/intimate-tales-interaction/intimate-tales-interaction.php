e8188421e2cb71ad699dc87abb5f59335a5e6311
Dawid Rogaczewski <mail@daro.de>
2023-08-14 22:44:43 +0200

    Plugin Header set

---
+++ src/IntimateTales/intimate-tales-interaction/intimate-tales-interaction.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Intimate Tales - Interaction Plugin
+ * Description: Manages interactions and real-time features for the IntimateTales platform.
+ * Version: 1.0.0
+ * Requires PHP: 8.0
+ * Author: Dawid Rogaczewski
+ * Author URI: https://www.intimate-tales.de
+ * Text Domain: intimate-tales-interaction
+ * Domain Path: /languages
+ */
---
+++ src/IntimateTales/intimate-tales-settings/intimate-tales-settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Intimate Tales - Settings Plugin
+ * Description: Manages general settings for the IntimateTales platform.
+ * Version: 1.0.0
+ * Requires PHP: 8.0
+ * Author: Dawid Rogaczewski
+ * Author URI: https://www.intimate-tales.de
+ * Text Domain: intimate-tales-settings
+ * Domain Path: /languages
+ */
