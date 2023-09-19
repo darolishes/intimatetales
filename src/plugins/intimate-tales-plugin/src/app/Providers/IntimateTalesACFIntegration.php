4cbfe06df73358e949464e381a0b4aa6d7e0a9fd
Dawid Rogaczewski <dawid@daro.de>
2023-09-19 13:59:38 +0200

    up

---
+++ src/plugins/intimate-tales-plugin/src/app/Providers/IntimateTalesACFIntegration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Roots\Acorn\ServiceProvider;
+
+class IntimateTalesACFIntegration extends ServiceProvider
+{
+    /**
+     * Registrieren Sie die ACF-Integrationsservices.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Hier können Sie ACF-Felder und -Einstellungen registrieren
+    }
+
+    /**
+     * Booten Sie die ACF-Integrationsservices.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Hier können Sie Aktionen und Filter für ACF hinzufügen
+    }
+}
