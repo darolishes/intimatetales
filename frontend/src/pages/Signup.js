c0643600539ad4be465bef3c561d36049458c678
Dawid Rogaczewski <dawid@daro.de>
2023-12-27 13:19:05 +0100

    up

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function Signup() {
   const [userInfo, setUserInfo] = useState({
@@ -6,40 +7,37 @@ function Signup() {
     email: '',
     password: ''
   });
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
     setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Implement functionality to handle signup (e.g., API call)
+    setError('');
+    setIsLoading(true);
+
+    try {
+      // Add validation logic here
+      
+      const response = await axios.post('/api/signup', userInfo);
+      // Handle success (e.g., redirect or show a success message)
+    } catch (error) {
+      setError('Signup failed: ' + error.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="username"
-        value={userInfo.username}
-        onChange={handleChange}
-        placeholder="Username"
-      />
-      <input
-        type="email"
-        name="email"
-        value={userInfo.email}
-        onChange={handleChange}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        name="password"
-        value={userInfo.password}
-        onChange={handleChange}
-        placeholder="Password"
-      />
-      <button type="submit">Sign Up</button>
+      {/* Form fields */}
+      {error && <p className="error">{error}</p>}
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? 'Signing Up...' : 'Sign Up'}
+      </button>
     </form>
   );
 }
